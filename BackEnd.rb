09f04b9b2f4f70f5a37dcd488e3dbfa3fe9a34b1
Electr0n <hunterlzp@yandex.ru>
2016-09-07 15:13:29 +0300

    BackEnd

---
+++ BackEnd.rb
@@ -0,0 +1,22 @@
+require 'sinatra'
+require 'json'
+
+get '/hi' do
+  "Hello world, it's #{Time.now} at the server!"
+end
+
+get '/dialog' do
+  age = request.params['age']
+  "I'm #{age} years old"
+end
+
+get '/json' do
+	content_type :json
+	guy = { name: "Eugen", age: "10"}
+	guy.to_json
+end
+
+post '/bye' do
+	"Bye"
+	a = JSON.parse(request.env).to_json
+end
